80c7177d87ae82dbc976424f83922aec0c4a5067
PreetMehta09 <preet.pw7@gmail.com>
2026-07-16 16:56:05 +0530

    Add files via upload

---
+++ MIniproject.py
@@ -0,0 +1,15 @@
+shopping = []
+
+shopping.append(input("Enter Fruit 1: "))
+shopping.append(input("Enter Fruit 2: "))
+shopping.append(input("Enter Fruit 3: "))
+shopping.append(input("Enter Fruit 4: "))
+shopping.append(input("Enter Fruit 5: "))
+
+print("Your shopping list:")
+print("")
+print("1.",shopping[0])
+print("2.",shopping[1])
+print("3.",shopping[2])
+print("4.",shopping[3])
+print("5.",shopping[4])
